6f6f598a54a7a7ca80949c41224389cbdd283046
Soeun Lee <thdms1005@gmail.com>
2023-05-06 18:03:43 +0200

    main page UI and animation

--- src/components/page/main/index.tsx
+++ src/components/page/main/index.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/layout/navbar';
 import styles from './main.module.css';
 import Image from 'next/image';
 import profilePic from '../../../../public/portfolio.jpeg';
+import arrowDown from '../../../../public/arrow_down.png';
 
 const Main = () => {
   return (
@@ -9,27 +10,31 @@ const Main = () => {
       <Navbar />
       <div className={styles.wrap}>
         <div className={styles.heroWrap}>
-          <span className={styles.heroText}>FRONTEND </span>
-          <span className={styles.heroText}>DEVELOPER </span>
-        </div>
-        <div className={styles.presentationWrap}>
-          <p className={styles.paragraph}>
-            Hello I&apos;m Soeun, frontend developer Lorem Ipsum is simply dummy
-            text of the printing and typesetting industry. Lorem Ipsum has been
-            &apos;s standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
-          </p>
+          <div className={styles.textWrap}>
+            <span className={styles.heroText}>FRONTEND </span>
+            <span className={styles.heroText}>DEVELOPER </span>
+            <p className={`${styles.paragraph} ${styles.reactP} `}>
+              - SPECIALISED IN REACT
+            </p>
+          </div>
           <Image
             src={profilePic}
             alt="profile photo"
             className={styles.photo}
           />
+        </div>
+        <div className={styles.presentationWrap}>
+          <div className={styles.scrollWrap}>
+            <Image src={arrowDown} alt="arrow" className={styles.arrow} />
+            <span>SCROLL</span>
+          </div>
+          <div className={styles.bottomWrap}>
+            <p className={styles.paragraph}>- SOON TO BE FULL-STACK</p>
+            <div className={styles.nameWrap}>
+              <span className={styles.name}>SOEUN</span>
+              <span className={styles.name}>LEE</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>

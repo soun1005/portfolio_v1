3c7b67bcc08bdbfc7259e245cc3531abbfe14d4f
Soeun Lee <thdms1005@gmail.com>
2023-05-16 14:24:22 +0200

    responsive mobile UI

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,15 +1,74 @@
 import styles from './Navbar.module.css';
 import Image from 'next/image';
 import icon from '@/assets/nav/sunlightIcon.png';
-// import Link from 'next/link';
 import { Link } from 'react-scroll/modules';
-import Skills from '@/components/page/skills';
 
 const Navbar = () => {
   return (
     <>
       <nav className={styles.container}>
         <div className={styles.logo}>SOEUN LEE</div>
+        {/* responsive nav */}
+        <div className={styles.responsiveNav}>
+          <div className={styles.buttonWrap}>
+            <div className={styles.openIcon}>open</div>
+            <div className={styles.closeIcon}>X</div>
+          </div>
+          <ul>
+            <li>
+              <Link
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={300}
+              >
+                SKILLS
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={300}
+              >
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={300}
+              >
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="footer"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={300}
+              >
+                LINKS
+              </Link>
+            </li>
+            <li>
+              <a href="mailto: thdms1005@gmail.com">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+        {/* nav desktop mode */}
         <div className={styles.menuWrap}>
           <ul className={styles.nav}>
             <li>

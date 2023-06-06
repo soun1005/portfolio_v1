c3d824c5c36d8979283996c20b59f6be33da8050
Soeun Lee <thdms1005@gmail.com>
2023-06-06 11:13:46 +0200

    cv and about page

--- src/components/page/about/index.tsx
+++ src/components/page/about/index.tsx
@@ -13,18 +13,14 @@ const About = () => {
           <p className={styles.title}>frontend developer</p>
           <p className={styles.title}>from S.Korea based in Paris, France</p>
           <p className={styles.desc}>
-            I studied International commerce at university and I&apos;m very
-            interested in marketing, so after university I worked for a little
-            while at a web marketing agency in Seoul. When I came to France, I
-            didn&apos;t speak French very well, so I worked for a Korean cargo
-            company for two years, and then I had the opportunity to learn
-            something I was very interested in, studying to be a developer.
-            Since I was kid, my ambition was to be a fashion designer and I had
-            plans to go to art school when I was high school student, but I had
-            to give it up when reality hit me. I love creating something out of
-            nothing, that is why I am so fascinated in coding. Learning
-            something is such a fun thing to do and I will never stop learning
-            for the rest of my life.
+            Hello, I&apos;m Soeun from South Korea. I studied International
+            Trade at university and worked as a junior project manager at a
+            marketing company in Seoul for a little while. After that, I worked
+            for a Korean logistics company in Paris, France for two years. I had
+            a great opportunity to start attending a school where I could learn
+            to be a developer, which is something I&apos;ve always wanted to do.
+            I have always enjoyed learning new things and creating something,
+            and I will never stop learning, just as I always have.
           </p>
         </div>
         <div className={styles.photoWrap}>
@@ -33,7 +29,7 @@ const About = () => {
       </div>
       <Button
         text="VIEW CV"
-        link="https://drive.google.com/file/d/17KuDVWw222NdkA_kTffH_4Px96Totl3M/view?usp=share_link"
+        link="https://drive.google.com/file/d/1i3_YSnXo7MY6C4De3eiTYUhLX2m7l9P6/view?usp=drive_link"
       />
     </div>
   );

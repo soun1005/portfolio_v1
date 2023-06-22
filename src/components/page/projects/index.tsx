e5affc4a8d6c21e9d3415de0ac698aa8ac994878
Soeun Lee <thdms1005@gmail.com>
2023-06-22 10:51:50 +0200

    project section devided

--- src/components/page/projects/index.tsx
+++ src/components/page/projects/index.tsx
@@ -1,11 +1,16 @@
 import ProjectCard from '@/components/common/projectCard';
 import styles from './projects.module.css';
 import data from '@/data/projects.json';
+import personalProjectData from '@/data/personalProjects.json';
 
 const Projects = () => {
   return (
     <div className={`container ${styles.container}`} id="projects">
-      <h1 className={`title ${styles.title}`}>Projects</h1>
+      <h1 className={`title ${styles.title}`}>Personal Projects</h1>
+      <div className={styles.cardContainer}>
+        <ProjectCard data={personalProjectData} />
+      </div>
+      <h1 className={`title ${styles.title}`}>School Projects</h1>
       <div className={styles.cardContainer}>
         <ProjectCard data={data} />
       </div>

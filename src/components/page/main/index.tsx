dd35c4072215a3d34b96aeb0989298c8aecec65a
Soeun Lee <thdms1005@gmail.com>
2023-07-09 15:27:24 +0200

    main page animation

--- src/components/page/main/index.tsx
+++ src/components/page/main/index.tsx
@@ -15,8 +15,28 @@ const Main = () => {
       <div className={styles.wrap}>
         <div className={styles.heroWrap}>
           <div className={styles.textWrap}>
-            <span className={styles.heroText}>FRONTEND </span>
-            <span className={styles.heroText}>DEVELOPER </span>
+            {/* <span className={styles.heroText}>FRONTEND </span>
+            <span className={styles.heroText}>DEVELOPER </span> */}
+            <div className={styles.firstRow}>
+              <span>F</span>
+              <span>R</span>
+              <span>O</span>
+              <span>N</span>
+              <span>T</span>
+              <span>E</span>
+              <span>N</span>
+              <span>D</span>
+            </div>
+            <div className={styles.secondRow}>
+              <span>D</span>
+              <span>E</span>
+              <span>V</span>
+              <span>E</span>
+              <span>L</span>
+              <span>P</span>
+              <span>E</span>
+              <span>R</span>
+            </div>
             <p className={`${styles.paragraph} ${styles.reactP} `}>
               - SPECIALISED IN REACT
             </p>
@@ -41,8 +61,18 @@ const Main = () => {
           <div className={styles.bottomWrap}>
             <p className={styles.paragraph}>- SOON TO BE FULL-STACK</p>
             <div className={styles.nameWrap}>
-              <span className={styles.name}>SOEUN</span>
-              <span className={styles.name}>LEE</span>
+              <span className={styles.nameFirstRow}>
+                <span>S</span>
+                <span>O</span>
+                <span>E</span>
+                <span>U</span>
+                <span>N</span>
+              </span>
+              <span className={styles.nameSecondRow}>
+                <span>L</span>
+                <span>E</span>
+                <span>E</span>
+              </span>
             </div>
           </div>
         </div>

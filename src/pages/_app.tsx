920b8e3a54197ffa0134c02fd9f142d3d1105eb9
Soeun Lee <thdms1005@gmail.com>
2023-09-22 19:36:10 +0200

    Google analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import AppContextProvider from './context/AppContext';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,6 +11,17 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <AppContextProvider>
       <Component {...pageProps} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script id="google-analytics">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+  `}
+      </Script>
     </AppContextProvider>
   );
 }

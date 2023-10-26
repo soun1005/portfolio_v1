35c204881d7bc5db955539dcd845b28198c0ccf9
Soeun Lee <thdms1005@gmail.com>
2023-10-26 16:43:56 +0200

    project tag component - radius and size / projects - dropdown added

--- src/components/page/projects/index.tsx
+++ src/components/page/projects/index.tsx
@@ -2,18 +2,130 @@ import ProjectCard from '@/components/common/projectCard';
 import styles from './projects.module.css';
 import data from '@/data/projects.json';
 import personalProjectData from '@/data/personalProjects.json';
+import { useState } from 'react';
+import Image from 'next/image';
+import up from '@/assets/up.png';
+import down from '@/assets/down.png';
+import up_gray from '@/assets/up_gray.png';
+import down_gray from '@/assets/down_gray.png';
+import { useAppContext } from '@/pages/context/AppContext';
 
 const Projects = () => {
+  const [personal, setPersonal] = useState(false);
+  const [school, setSchool] = useState(false);
+
+  const { isLightMode } = useAppContext();
+
+  const controlDropdown = (e: React.MouseEvent) => {
+    const target = e.target as HTMLElement;
+    const value = target;
+
+    // two constrains for title(h1) and arrow
+    if (
+      value.innerText === 'Personal Projects' ||
+      target.parentElement?.innerText === 'Personal Projects'
+    ) {
+      setPersonal(!personal);
+    }
+
+    if (
+      value.innerText === 'School Projects' ||
+      target.parentElement?.innerText === 'School Projects'
+    ) {
+      setSchool(!school);
+    }
+  };
+
   return (
     <div className={`container ${styles.container}`} id="projects">
-      <h1 className={`title ${styles.title}`}>Personal Projects</h1>
-      <div className={styles.cardContainer}>
-        <ProjectCard data={personalProjectData} />
-      </div>
-      <h1 className={`title ${styles.title}`}>School Projects</h1>
-      <div className={styles.cardContainer}>
-        <ProjectCard data={data} />
-      </div>
+      <h1
+        className={`title ${styles.title} ${styles.personalProjectDropdown}`}
+        onClick={controlDropdown}
+      >
+        Personal Projects
+        {isLightMode ? (
+          personal ? (
+            <Image
+              src={up}
+              alt="arrowUp"
+              className={styles.arrowUp}
+              priority={true}
+              onClick={controlDropdown}
+            />
+          ) : (
+            <Image
+              src={down}
+              alt="arrowDown"
+              className={styles.arrowDown}
+              priority={true}
+            />
+          )
+        ) : personal ? (
+          <Image
+            src={up_gray}
+            alt="arrowUp"
+            className={styles.arrowUp}
+            priority={true}
+          />
+        ) : (
+          <Image
+            src={down_gray}
+            alt="arrowDown"
+            className={styles.arrowDown}
+            priority={true}
+          />
+        )}
+      </h1>
+
+      {personal ? (
+        <div className={styles.cardContainer}>
+          <ProjectCard data={personalProjectData} />
+        </div>
+      ) : (
+        ''
+      )}
+
+      <h1 className={`title ${styles.title}`} onClick={controlDropdown}>
+        School Projects
+        {isLightMode ? (
+          school ? (
+            <Image
+              src={up}
+              alt="arrowUp"
+              className={styles.arrowUp}
+              priority={true}
+            />
+          ) : (
+            <Image
+              src={down}
+              alt="arrowDown"
+              className={styles.arrowDown}
+              priority={true}
+            />
+          )
+        ) : school ? (
+          <Image
+            src={up_gray}
+            alt="arrowUp"
+            className={styles.arrowUp}
+            priority={true}
+          />
+        ) : (
+          <Image
+            src={down_gray}
+            alt="arrowDown"
+            className={styles.arrowDown}
+            priority={true}
+          />
+        )}
+      </h1>
+      {school ? (
+        <div className={styles.cardContainer}>
+          <ProjectCard data={data} />
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
